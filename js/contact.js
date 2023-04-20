85820414f49ff8362255287a66c8d136ef3b04ff
mateoosz <125757166+mateoosz@users.noreply.github.com>
2023-04-20 13:58:47 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -19,12 +19,12 @@ contactForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     
-    const last = contactForm['name'].value;
+    const last = contactForm['last'].value;
     const email = contactForm['email'].value;
     const message = contactForm['message'].value;
     database.ref('contact').push({
         
-        name: last,
+        last: last,
         email: email,
         message: message
     });
