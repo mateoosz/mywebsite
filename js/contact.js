6a367ef56f41a6bc6b711427f1404e2c1c9104a8
mateoosz <125757166+mateoosz@users.noreply.github.com>
2023-04-20 03:48:29 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,5 @@
-
-
-
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBI44dObKM8MNQx2rY9l3VSXFGnmYTKr_g",
+   apiKey: "AIzaSyBI44dObKM8MNQx2rY9l3VSXFGnmYTKr_g",
     authDomain: "contact-8a280.firebaseapp.com",
     databaseURL: "https://contact-8a280-default-rtdb.europe-west1.firebasedatabase.app/",
     projectId: "contact-8a280",
@@ -11,28 +7,27 @@ const firebaseConfig = {
     messagingSenderId: "356571204183",
     appId: "1:356571204183:web:aa6c157c3ae25563ff7112",
     measurementId: "G-BP3VZJXD79"
-  };
-  
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
-  
-  
-  const contactForm = document.querySelector('#contact-form');
-  
-  contactForm.addEventListener('submit', (event) => {
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+const contactForm = document.querySelector('#contact-form');
+
+contactForm.addEventListener('submit', (event) => {
     event.preventDefault();
-  
+
     const name = contactForm['first'].value;
     const last = contactForm['last'].value;
     const email = contactForm['email'].value;
     const message = contactForm['message'].value;
     database.ref('contact').push({
-  first: first,
-  last: last,
-  email: email,
-  message: message
-});
-  
+        first: name,
+        last: last,
+        email: email,
+        message: message
+    });
+
     contactForm.reset();
-  });
+});
